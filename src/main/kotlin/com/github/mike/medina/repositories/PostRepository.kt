595eef4707ed47626508509e945f06c742e929aa
Mike Medina <mikemedina805@gmail.com>
2018-08-04 15:58:18 -0400

    Add back accidentally nuked code

--- src/main/kotlin/com/github/mike/medina/repositories/PostRepository.kt
+++ src/main/kotlin/com/github/mike/medina/repositories/PostRepository.kt
@@ -1,2 +1,9 @@
 package com.github.mike.medina.repositories
 
+import com.github.mike.medina.entities.Post
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+
+@RepositoryRestResource(collectionResourceRel = "posts", path = "posts")
+interface PostRepository : PagingAndSortingRepository<Post, Long>
