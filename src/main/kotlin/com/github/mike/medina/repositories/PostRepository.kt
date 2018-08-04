10112255992060d07d3eb04c69777c5d44a64a93
Mike Medina <mikemedina805@gmail.com>
2018-08-04 18:24:42 -0400

    Fix CORS configuration

--- src/main/kotlin/com/github/mike/medina/repositories/PostRepository.kt
+++ src/main/kotlin/com/github/mike/medina/repositories/PostRepository.kt
@@ -3,7 +3,9 @@ package com.github.mike.medina.repositories
 import com.github.mike.medina.entities.Post
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.web.bind.annotation.CrossOrigin
 
 
+@CrossOrigin("http://localhost:8000")
 @RepositoryRestResource(collectionResourceRel = "posts", path = "posts")
 interface PostRepository : PagingAndSortingRepository<Post, Long>
